ad719ddf68afe4e692e683e7c6220b2030ed8452
niranjan122005 <niranjanv@thestackly.com>
2026-07-21 11:43:12 +0530

    Variables, Datatypes and Type Casting

--- Intro.py
+++ Intro.py
@@ -1,3 +1,14 @@
-print(77)
-print(66)
-print(7.7)
+# these are the printing statements
+# ''these are the printing statements
+# these are the printing statements
+# these are the printing statements
+# these are the printing statements
+# these are the printing statements
+# these are the printing statements''
+
+# print(77)
+# print(66)
+# print(7.7)
+# print("Hello")
+
+input()
---
+++ VarDatatypesTypeCasting.py
@@ -0,0 +1,40 @@
+# a1=10
+# print(a1)
+
+# a=10
+# a=12
+# print(a)
+
+# a=10
+# b=3.14
+# c='I am "Leo"'
+
+# print(type(c)) #<class 'str'>
+# print(c)
+
+# print(4+6.5)
+
+# print("Hello"+" "+"world")
+# print("44"+"55")
+
+# print("Hello"+5)
+
+# age="Leo"
+# age=44
+
+# I am Leo. My age is 44.
+# print("I am "+age+". My age is "+str(age)+".")
+
+# d=True
+# print(type(d))
+
+# age=input("Enter Your Age: ") # 55 "55"
+# print(type(age))
+# print(age)
+# print(age)
+# print(age)
+
+# inp=int(input("Enter any number: "))
+# print(inp+100)
+# print(inp-100)
+# print(inp*100)
