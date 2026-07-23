28bbb9ed3037b65e98356b09971ae953dc9ce2f3
niranjan122005 <niranjanv@thestackly.com>
2026-07-23 11:55:18 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,59 @@
+# Arith 
+
+a=55
+b=3
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(3**3)
+#4.5566 --> 4
+
+# print(2+3-4*8/2**2)
+
+# Comparison
+
+# print(9==-9)
+# print(9!=9)
+# print(9<9)
+# print(10>9)
+# print(5<=9)
+# print(5>=9)
+
+# Logical
+
+# a=True 
+# b=False
+
+# print(a and b)
+# print(a or b)
+
+# print(not not a)
+
+# Assign
+# a=10 #10
+# a+=15 #25
+# # a+=5
+# a-=5 #20
+# a*=5
+# a/=25 #4
+# a//=2 #2
+# a%=30 #2
+# a**=3 #8
+
+# print(a) 
+
+# Bitwise 
+
+# print(69&38)
+# print(69|38)
+# print(~3)
+# print(33^15)
+# print(33<<2)
+
+# print(~13)
+print(~60)
