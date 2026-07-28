61b02634f3519c2ae9a085e94d7fb5e287e15393
niranjan122005 <niranjanv@thestackly.com>
2026-07-28 11:45:17 +0530

    Conditional Clause

---
+++ ControlFlow.py
@@ -0,0 +1,73 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# age=int(input("Enter Your Age: "))
+
+# if age>=18:
+#     print("You can Vote...!")
+# else:
+#     print("You cannot Vote...!")
+
+# a=10
+
+# if a<=10:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print("-------------")
+# else:
+#     print("From Else Block")
+
+# print("Final Step...!")
+
+
+# b=9
+
+# if b<9:
+#     print("From If Block")
+# elif b>9:
+#     print("From Elif1 Block")
+# elif b<9:
+#     print("From Elif2 Block")
+# else:
+#     print("From else block")
+
+# accType="Current"
+# Balance=1001
+
+# if accType=="Current" and Balance>1000:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+# mark = int(input("Enter your marks (0-100): "))
+# if mark >= 90 and mark <= 100:
+#     print("Grade: O")
+# elif mark >= 80:
+#     print("Grade: A+")
+# elif mark >= 70:
+#     print("Grade: A")
+# elif mark >= 60:
+#     print("Grade: B")
+# elif mark >= 50:
+#     print("Grade: C")
+# elif mark >= 0:
+#     print("Grade: Fail")
+# else:
+#     print("Invalid Marks")
+
+# inp=int(input("Enter a number: "))
+
+# if inp>10:
+#     if inp>15:
+#         print("Very Large")
+#     else:
+#         print("Large")
+# else:
+#     if inp<5:
+#         print("Very Small")
+#     else:
+#         print("Small")
