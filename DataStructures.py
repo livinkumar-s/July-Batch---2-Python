f33ba8ac60195b3406ca46e4f80ae18b5fdf7bc4
niranjan122005 <niranjanv@thestackly.com>
2026-08-06 11:51:36 +0530

    Data Structures - List, Tuple

---
+++ DataStructures.py
@@ -0,0 +1,56 @@
+# List 
+
+# l1=[1,2,3,9.9,"Hello",False,1]
+# print(len(l1))
+# print(l1[2])
+# l1[2]=5
+# l1.append(55)
+# l1.append(44)
+# l1.insert(-2,-1)
+# l1.extend([1,2,3,4,5])
+# l1.remove(9.9)
+# l1.remove(1)
+# l1.pop(4)
+# l1.pop()
+
+# print(l1.index("hello"))
+# l1=[1,2,3,9.9,234,342,1,2,0,-1]
+# print(l1.count("hello"))
+# l1.reverse()
+# l1.sort(reverse=True)
+# print(l1)
+
+# a=3214234
+
+# for i in a:
+#     print(i)
+
+# iterable + ordered ---> Sequence
+
+# Tuple 
+
+# t1=(1,2,3,1,2,3,"Hello")
+# # t1[2]=33
+# print(t1.index(4))
+
+# person=["Leo",23,"SASE"]
+# name,age,role=person
+
+# print(role)
+
+# a=1,2,3,4,5 #Tuple
+# print(a)
+
+# a,b=1,2
+
+# Slicing
+
+t1="Hello World"
+
+# print(t1[5:])
+# print(t1[2:-1]) #2,3
+# print(t1[2:6:2]) #2,4
+# print(t1[:4])
+# print(t1[::-1])
+
+# 
