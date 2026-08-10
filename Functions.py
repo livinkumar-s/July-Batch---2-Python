bb51b7a42aad29cb72b5a022346b9c4fc6a2ccbd
niranjan122005 <niranjanv@thestackly.com>
2026-08-10 12:03:49 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,65 @@
+# def printSteps():
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     return 33
+#     print("Step4")
+#     print("Step5")
+
+# a=printSteps()
+# print(a+10)
+# printSteps()
+# printSteps()
+# printSteps()
+# printSteps()
+# printSteps()
+# printSteps()
+# printSteps()
+
+# def add(a,b,c=10):
+#     print(a)
+#     print(b)
+#     print(c)
+#     print(a+b+c)
+
+# add(2,3,2)
+# add(-1,20,-70)
+# add(c=3,b=4,a=5)
+
+# add(3,4,100)
+
+# def addAll(*a):
+#     ans=0
+#     for i in a:
+#         ans+=i
+#     print(ans)
+
+# addAll(1,2,3,4,5,6)
+
+# def getDetails(**a):
+#     print(a)
+
+# getDetails(name="Hendry",age=44)
+
+
+# def add(a,b):
+#     return a+b
+
+# print(add(4,4))
+
+# None
+
+# print(add(3,add(4,add(9,add(3,-1)))))
+
+# add(45,54)
+
+# print(34)
+
+# a=input("Enter your name: ")
+
+def dummy():
+    a=10
+    print(a)
+
+dummy()
+print(a)
