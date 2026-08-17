5cf1fcaab97d9152e27d963c314f34b586afbcc5
niranjan122005 <niranjanv@thestackly.com>
2026-08-17 11:49:29 +0530

    Exception Handling

---
+++ ExceptionHandling.py
@@ -0,0 +1,39 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# if True:
+#     print(1)
+
+# print("Hello)
+
+# print("Hello"+1)
+# print(int("Hello"))
+
+# try:
+#     print("hello"+"45")
+#     print(6/0)
+# except ZeroDivisionError:
+#     print("You cannot divide that number by 0")
+# except TypeError:
+#     print("Cannot add a str with number")
+
+# try:
+#     inp1=int(input("Enter Your 1st Number: "))
+#     inp2=int(input("Enter Your 2nd Number: "))
+#     print(inp1/inp2)
+# except ZeroDivisionError:
+#     print("You cannot divide that number by 0")
+# except ValueError:
+#     print("Invalid Input...!")
+# finally:
+#     print("Done")
+
+i=1
+
+while i<=100:
+    print(i)
+    # i+=1
+
